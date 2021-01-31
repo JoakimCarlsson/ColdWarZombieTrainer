d63edec1adc91100dab70bee20505740e3878a8f
Joakim Carlsson <joakimcarlsson1994@gmail.com>
2021-01-31 06:16:12 +0100

    Update offsets and added more wepaons too the dropdown list.

--- ColdWarZombieTrainer/Offsets.cs
+++ ColdWarZombieTrainer/Offsets.cs
@@ -6,23 +6,21 @@ namespace ColdWarZombieTrainer
     {
         public static int PlayerBase = 0xFC297B8;
         public static int ZMXPScaleBase = 0xFC517B0;
-        public static int TimeScaleBase = 0xECF9C74 + 0x7C;
+        public static int TimeScaleBase = 0xECF9C74;
 
         public static class PlayerCompPtr
         {
             public static int ArraySizeOffset = 0xB830;
-            public static int InfraredVision = 0xE66; //On=0x10|Off=0x0
+            public static int InfraredVision = 0xE66;
             public static int GodMode = 0xE67;
             public static int RunSpeed = 0x5C30;
             public static int Ammo = 0x13D4;
             public static int Points = 0x5CE4;
             public static int RapidFire1 = 0xE6C;
             public static int RapidFire2 = 0xE80;
-            public static int Crit = 0x10CC;
             public static int Name = 0x5BDA;
             public static int CurrentUsedWeaponID = 0x28;
             public static int SetWeaponID = 0xB0;
-            public static int JumpHeight = 0xFE63458; //gamebase + 0xFE63458 (pointer) + 0x130 (default value 39.0float)
         }
 
 
